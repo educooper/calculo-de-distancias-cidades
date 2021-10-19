dc19390e897457f464994139d094e4b9b0c1ab79
Eduardo Cupertino <37818675+educooper@users.noreply.github.com>
2021-10-19 12:37:33 -0300

    delete to re create

--- src/main/java/com/github/educooper/citiesapi/countries/repositories/CountryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.educooper.citiesapi.countries.repositories;
-
-import com.github.educooper.citiesapi.countries.entities.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryRepository extends JpaRepository<Country, Long> {
-}
--- src/main/java/com/github/educooper/citiesapi/states/repositories/StateRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.educooper.citiesapi.states.repositories;
-
-import com.github.educooper.citiesapi.states.entities.State;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StateRepository extends JpaRepository<State, Long> {
-}
